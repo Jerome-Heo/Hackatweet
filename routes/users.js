46287eaa3b5745a578827b92bcabecc665b597ab
jérôme <jplusquellec@gmail.com>
2024-07-18 11:56:46 +0200

    hackatweetback

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,65 @@
 var express = require('express');
 var router = express.Router();
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require("uid2")
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+//Inscription + vérif de la saisie
+
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+  
+  //Pas de user trouvé donc password hashé + token(32) 
+
+  User.findOne({ username: req.body.username }).then(data => {
+
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const newUser = new User({
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+      });
+
+      //Création du user dans BDD ou message d'erreur s'il existe déjà
+
+      newUser.save().then((newDoc) => {
+        res.json({ result: true, token: newDoc.token, username: req.body.username });
+      });
+    } else {
+
+      res.json({ result: false, error: 'User already exists' });
+    }
+  });
+});
+
+//Connexion d'un user déjà existant + vérif de la saisie. 
+
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  //User trouvé dans la BDD avec les champs correspondants
+
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token, username: req.body.username });
+    } else {
+      res.json({ result: false, error: 'User not found' });
+    }
+  });
+});
+
+
+
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
